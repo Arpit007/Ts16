385eb27542df72c72b8c11cb549642f97400f153
Arpit007 <arpit2011@live.com>
2016-09-20 01:31:01 +0530

    working

--- app/src/main/java/com/nitkkr/gawds/ts16/mainActivity.java
+++ app/src/main/java/com/nitkkr/gawds/ts16/mainActivity.java
@@ -1,8 +1,12 @@
 package com.nitkkr.gawds.ts16;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Size;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,10 +15,20 @@ import android.support.v4.view.ViewPager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import javax.crypto.NullCipher;
 
 public class mainActivity extends AppCompatActivity
 		implements NavigationView.OnNavigationItemSelectedListener
@@ -28,7 +42,12 @@ public class mainActivity extends AppCompatActivity
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-		toolbar.setTitle("Talent Show'16");
+		TextView view=(TextView)toolbar.findViewById(R.id.mainLogo);
+
+		Typeface font = Typeface.createFromAsset(getAssets(),
+				"fonts/FREESCPT.TTF");
+		view.setTypeface(font);
+
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
 				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
