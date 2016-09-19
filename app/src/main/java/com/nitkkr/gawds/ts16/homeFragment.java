3cd4e1378de9f9cd0f97f1abbf100eafb646558e
Arpit007 <arpit2011@live.com>
2016-09-20 02:35:43 +0530

    working

--- app/src/main/java/com/nitkkr/gawds/ts16/homeFragment.java
+++ app/src/main/java/com/nitkkr/gawds/ts16/homeFragment.java
@@ -2,6 +2,7 @@ package com.nitkkr.gawds.ts16;
 
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -21,9 +23,14 @@ public class homeFragment extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState)
 	{
+		Typeface font = Typeface.createFromAsset(activity.getAssets(),
+				"fonts/Font1.ttf");
+
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_home, container, false);
-		view.findViewById(R.id.newsButton).setOnClickListener(new View.OnClickListener()
+
+		Button button=(Button)view.findViewById(R.id.newsButton);
+		button.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View v)
@@ -31,7 +38,10 @@ public class homeFragment extends Fragment
 				((mainActivity)activity).navigateToTab(1);
 			}
 		});
-		view.findViewById(R.id.liveButton).setOnClickListener(new View.OnClickListener()
+		button.setTypeface(font);
+
+		button=(Button)view.findViewById(R.id.liveButton);
+		button.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View v)
@@ -39,7 +49,10 @@ public class homeFragment extends Fragment
 				((mainActivity)activity).navigateToTab(2);
 			}
 		});
-		view.findViewById(R.id.upcomingButton).setOnClickListener(new View.OnClickListener()
+		button.setTypeface(font);
+
+		button=(Button)view.findViewById(R.id.upcomingButton);
+		button.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View v)
@@ -47,7 +60,10 @@ public class homeFragment extends Fragment
 				((mainActivity)activity).navigateToTab(3);
 			}
 		});
-		view.findViewById(R.id.scheduleButton).setOnClickListener(new View.OnClickListener()
+		button.setTypeface(font);
+
+		button=(Button)view.findViewById(R.id.scheduleButton);
+		button.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View v)
@@ -56,6 +72,7 @@ public class homeFragment extends Fragment
 				startActivity(intent);
 			}
 		});
+		button.setTypeface(font);
 		return view;
 	}
 
